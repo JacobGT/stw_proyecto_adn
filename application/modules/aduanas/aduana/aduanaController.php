625802a24a3b9a240897b8de204b3ac8e03a005a
chevskouski <kesectrl@duck.com>
2023-04-13 11:03:32 -0600

    Update adunaController v-0.02
    Se agrego la funcion para generar pdf, dentro de la misma clase, para la realizacion de pruebas y formateo del pdf.

--- application/modules/aduanas/aduana/aduanaController.php
+++ application/modules/aduanas/aduana/aduanaController.php
@@ -54,6 +54,77 @@ class aduanaController extends Controller{
         return $datos;
     }
 
+    public function generar_poliza($array_resultado){
+        /*$this->modelo->id_tabla = 'id_producto';
+        $this->modelo->tabla = 'opr_carrito';
+        $params=array('operacion'=>'consultar_productos_carrito');
+        $resultado =$this->modelo->ejecutar_sql($params);
+        */
+        $datos = $array_resultado;            
+        $nurmeroDatos = sizeof($datos);
+
+        $pdf = new FPDF();
+        $pdf->AddPage();
+        $pdf->SetTitle('Poliza ADUANERA');
+        $pdf->SetFont('Arial','B',16);
+        
+        //Header
+        // Arial bold 15
+        $pdf->SetFont('Arial','B',20);
+        // Movernos a la derecha
+        $pdf->Cell(70);
+        // Título
+        $pdf->Cell(58,10,'Detalles de la POLIZA',0,0,'C');
+        // Salto de línea
+        $pdf->Ln(20);
+        $fechaactual = 'Fecha: '.date('d/m/Y');
+        $pdf->SetFont('Arial','',12);
+        $pdf->Cell(0,10,$fechaactual,0,0,'');
+        //$pdf->Image(BASE_URL.'layout/store/assets/img/logo/logo.png',150,35,40);
+        $pdf->Ln(5);
+        $pdf->Cell(0,10,'email: info@uspgcoders.site',0,0,'');
+        $pdf->Ln(5);
+
+        $poliza_no = 'generarPoliza';
+        $pdf->Cell(0,10,'No. Orden: '.$poliza_no,0,0,'');
+
+        $pdf->Ln(20);
+        $pdf->SetFont('Arial','B',15);
+        // Colores de los bordes, fondo y texto
+        $pdf->SetDrawColor(0,80,180);
+        $pdf->SetFillColor(189,236,182);
+        $pdf->Cell(160,10,'Nombre del Producto',1,0,'C',true);
+        $pdf->Cell(30,10,'Precio',1,1,'C',true);
+        
+
+
+
+        $total_prodcutos = 0;
+        $pdf->SetFont('Arial','',12);
+        $pdf->SetFillColor(226,240,251);
+        /*for ($x = 0; $x < $nurmeroDatos; $x++) {
+            $datosProducto = $datos[$x];
+            
+            $pdf->Cell(160,10,$datosProducto['descripcion'],1,0,'',true);
+            $pdf->Cell(30,10,'$ '.$datosProducto['precio'],1,1,'C',true);
+            $total_prodcutos = $total_prodcutos + floatval($datosProducto['precio']);
+        }*/
+        $pdf->SetFont('Arial','B',15);
+        $pdf->Cell(160,10,'Total',1,0,'R',0);
+        $pdf->SetFillColor(253,238,238);
+        $pdf->Cell(30,10,'$ '.sprintf('%0.2f',$total_prodcutos),1,1,'C',true);
+        
+        
+        // Posición: a 1,5 cm del final
+        $pdf->SetY(266);
+        // Arial italic 8
+        $pdf->SetFont('Arial','I',8);
+        // Número de página
+        $pdf->Cell(0,10,'Gracias por tu compra!',0,0,'C');
+        
+        $pdf->Output();
+    } 
+
     public function procesarDatos(){
         //$consulta = new webServices_consulta();
 
@@ -91,12 +162,13 @@ class aduanaController extends Controller{
             $manifiesto_procesado[$clave]["valor_total"] = $resultado[$clave]["valor_producto"] + ($resultado[$clave]["valor_producto"] * $procentaje_arancel);
             $manifiesto_procesado[$clave]["color_paquete"] = $color;
         }
-
+        //$this->generar_poliza($manifiesto_procesado);
+  
         return $manifiesto_procesado;
-
     }
 }
 
-    $procesard = new aduanaController();
-    var_dump($procesard -> procesarDatos());
+$procesard = new aduanaController();
+echo json_encode($procesard -> procesarDatos());
+
 ?>
